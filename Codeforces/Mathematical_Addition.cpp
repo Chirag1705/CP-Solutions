31ef81a814dd7959ffea7e1dc0ff3f6ce93e2e63
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2021-11-17 20:07:14 +0530

    Add files via upload

---
+++ Codeforces/Mathematical_Addition.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size()) 
+#define N 2e5+7
+
+void solve(){
+        ll u, v;
+        cin >> u >> v;
+
+        ll x = -(u * u);
+        ll y = v * v;
+
+        cout << x << " " << y << endl;
+}
+
+int main()
+{
+    FASTIO;
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+     solve();
+    }
+}
