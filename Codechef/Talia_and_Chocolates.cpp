e4697c82b1be578e094b0a3b3d0376920dac54c9
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2021-11-19 22:24:01 +0530

    Add files via upload

---
+++ Codechef/Talia_and_Chocolates.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size()) 
+#define N 2e5+7
+
+int fact(int n)
+{
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+
+int nCr(int n, int r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+void solve(){
+        ll n, k;
+        cin >> n >> k;
+
+         if(n==1){
+            cout<<1<<endl;
+            return;
+        }
+
+        if(k==1){
+            cout << n << endl;
+            return;
+        }
+
+        ll ans = nCr(n+k-1, n-1);
+
+        cout << ans << endl;
+}
+
+int main()
+{
+    FASTIO;
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+     solve();
+    }
+}
