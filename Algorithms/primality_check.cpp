e7dbe9036d94680e0adcd00261e31cc181850be3
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-01-25 11:24:07 +0530

    Add files via upload

---
+++ Algorithms/primality_check.cpp
@@ -0,0 +1,25 @@
+// Counting the number of divisors in O(sqrt(n)) times
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    
+    ll n;
+    cin >> n;
+
+    ll cnt = 0;
+
+    for(ll i = 1; i * i <= n; i++){
+        if(n%i==0)
+            cnt++;
+
+        if(i!=n/i)
+            cnt++;
+    }
+
+    cout << cnt << "\n";
+    
+    return 0;
+}
