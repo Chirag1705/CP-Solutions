b5c3f103f5dafe49dcacaa73bd4c7d6444cdf72c
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-02-10 00:25:22 +0530

    Add files via upload

---
+++ Codechef/Starters 19/Indivisible_Permutation.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size()) 
+#define N 2e5+7
+#define mod 1e9 + 7
+
+
+ll mod_mul(ll a, ll b, ll m = mod){
+    a = a%m;
+    b = b%m;
+
+    return (((a * b) % m) + m) % m;
+}
+
+ll modulo(ll a, ll b, ll n = mod){
+    ll x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n; // multiplying with base
+        }
+        y = (y*y) % n; // squaring the base
+        b /= 2;
+    }
+    return x % n;
+}
+
+void solve(){
+        ll n;
+        
+        cin >> n;
+
+        vt<ll>arr(n);
+
+        arr[n-1] = 1;
+
+        for(ll i = 0; i < n-1; i++){
+            arr[i] = i+2;
+        }
+
+        for(ll i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+
+        cout << "\n";
+}
+
+int main()
+{
+    FASTIO;
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+     solve();
+    }
+}
