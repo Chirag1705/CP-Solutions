f61d6c8990c5d2bd81a3866c48e13fa4e018873c
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-02-17 00:54:59 +0530

    Add files via upload

---
+++ Codeforces/Round 771 Div 2/Inversion_graph.cpp
@@ -0,0 +1,173 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size()) 
+#define N 2e5+7
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+
+ll inv(ll i) {
+    if (i == 1) 
+    return 1; 
+    
+    return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;
+    }
+
+ll mod_add(ll a, ll b) {
+    a = a % mod; 
+    b = b % mod; 
+    
+    return (((a + b) % mod) + mod) % mod;
+}
+
+
+ll mod_mul(ll a, ll b, ll m = mod){
+    a = a%m;
+    b = b%m;
+
+    return (((a * b) % m) + m) % m;
+}
+
+ll mod_sub(ll a, ll b, ll m = mod){
+    a = a%m;
+    b = b%m;
+
+    return((a - b) + m)%m;
+}
+
+// another binpow method
+ll pw(ll a, ll b, ll m){
+    if(b == 0)
+        return 1;
+    
+    if(b%2==0){
+        ll t = pw(a, b/2, m);
+        return (1LL * t * t % m);
+    }
+    else{
+        ll t = pw(a, (b-1)/2, m);
+        t = (1LL * t * t)%m;
+        return (1LL * a * t % m);
+    }
+}
+
+ll mod_div(ll a, ll b, ll m = mod){
+    a = a%m;
+
+    ll inv_b = pw(b, m-2, m);
+    // inverse of b = b^m-2%m, only for prime m
+
+    ll res = a*inv_b % m;
+
+    return res;
+}
+
+// binary exponentiation
+// binpow
+// O(log(b))
+ll modulo(ll a, ll b, ll n = mod){
+    ll x=1, y=a; 
+    while (b > 0) {
+        if (b%2 == 1) {
+            x = (x*y) % n; // multiplying with base
+        }
+        y = (y*y) % n; // squaring the base
+        b /= 2;
+    }
+    return x % n;
+}
+// binpow again
+ll pwr(ll a, ll b) {
+    a %= mod; 
+    ll res = 1; 
+
+    while (b > 0) {
+        if (b & 1) 
+            res = res * a % mod; 
+        a = a * a % mod; 
+        b >>= 1;
+    } 
+        return res;
+}
+
+
+
+void solve(){
+    // 1st Approach ->
+    ll n;
+    cin >> n;
+
+    vt<ll>arr(n);
+
+    for(ll i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    set<ll>s;
+    s.insert(0);
+    for(ll i = 1; i <= n; i++){
+            s.insert(-1*i);
+    }
+
+    ll ans = 0, target = n, maxa = n, last = n;
+
+    for(ll i = n-1; i >= 0; i--){
+        s.erase(-1*arr[i]);
+
+        if(arr[i] == target){
+            ll k = (-1)*(*s.begin());
+
+            if(k == (maxa-(last - i)))
+                ans++, target = k, maxa = k, last = i;
+            else
+                target = k;
+        }
+    }
+
+    cout << ans << "\n";
+
+    // 2nd Approach ->
+
+   /*  ll n; cin >> n;
+        vll a(n+1,0);
+        fo(i,1,n)
+        {
+            cin>>a[i];
+        }
+        vll mn_suffix(n+1),mx_prefix(n+1);
+        fo(i,1,n)
+        {
+            mx_prefix[i] = max(mx_prefix[i-1],a[i]);
+        }
+        mn_suffix[n] = a[n];
+        for(i = n-1;i>0;i--)
+        {
+            mn_suffix[i] = min(mn_suffix[i+1],a[i]);
+        }
+        fo(i,1,n)
+        {
+            if(i == n or mx_prefix[i] < mn_suffix[i+1])ans++;
+        }
+        cout<<ans<<"\n";
+    } */
+}
+
+int main()
+{
+    FASTIO;
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+     solve();
+    }
+}
