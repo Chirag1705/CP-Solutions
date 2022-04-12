e04508f4c9d2b7f033e253481273c72f71c192e3
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-04-12 11:59:58 +0530

    Create Priority_Queue.cpp

---
+++ Algorithms/Priority_Queue.cpp
@@ -0,0 +1,6 @@
+By Default C++ creates a max heap for priority queue.
+
+Syntax For Min Heap -> priority_queue<int, vector<int>, greater<int>> q;
+
+# Great Tip :-
+The above syntax may be difficult to remember, so in case of numeric values, we can multiply the values with -1 and use max heap to get the effect of min heap.
