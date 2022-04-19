baa21e361a468f4b7502e5d2a9f77ba60953fade
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-04-20 01:15:29 +0530

    Add files via upload

---
+++ Codeforces/Round 708 Div 2/M_Arrays.cpp
@@ -0,0 +1,173 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define N 2e5+7
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+
+ll inv(ll i) {
+	if (i == 1)
+		return 1;
+
+	return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;
+}
+
+ll mod_add(ll a, ll b) {
+	a = a % mod;
+	b = b % mod;
+
+	return (((a + b) % mod) + mod) % mod;
+}
+
+
+ll mod_mul(ll a, ll b, ll m = mod) {
+	a = a % m;
+	b = b % m;
+
+	return (((a * b) % m) + m) % m;
+}
+
+ll mod_sub(ll a, ll b, ll m = mod) {
+	a = a % m;
+	b = b % m;
+
+	return ((a - b) + m) % m;
+}
+
+// another binpow method
+ll pw(ll a, ll b, ll m) {
+	if (b == 0)
+		return 1;
+
+	if (b % 2 == 0) {
+		ll t = pw(a, b / 2, m);
+		return (1LL * t * t % m);
+	}
+	else {
+		ll t = pw(a, (b - 1) / 2, m);
+		t = (1LL * t * t) % m;
+		return (1LL * a * t % m);
+	}
+}
+
+ll mod_div(ll a, ll b, ll m = mod) {
+	a = a % m;
+
+	ll inv_b = pw(b, m - 2, m);
+	// inverse of b = b^m-2%m, only for prime m
+
+	ll res = a * inv_b % m;
+
+	return res;
+}
+
+// binary exponentiation
+// binpow
+// O(log(b))
+ll modulo(ll a, ll b, ll n = mod) {
+	ll x = 1, y = a;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			x = (x * y) % n; // multiplying with base
+		}
+		y = (y * y) % n; // squaring the base
+		b /= 2;
+	}
+	return x % n;
+}
+// binpow again
+ll pwr(ll a, ll b) {
+	a %= mod;
+	ll res = 1;
+
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return res;
+}
+
+
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+
+	vt<ll>arr(n);
+	set<ll>s;
+	unordered_map<ll, ll>mp;
+	ll cnt = 0;
+
+	for (ll i = 0; i < n; i++) {
+		cin >> arr[i];
+		arr[i] = arr[i] % m;
+		mp[arr[i]]++;
+	}
+
+	for (ll i = 0; i < n; i++) {
+		if (mp[m - arr[i]] and arr[i] <= m / 2 and arr[i] != 0)
+			s.insert(arr[i]);
+		else if (mp[m - arr[i]] == 0 and arr[i] != 0)
+			cnt++;
+	}
+
+	for (auto i : s) {
+		if (mp[m - i] == mp[i]) {
+			cnt++;
+		}
+		else {
+			cnt += abs(mp[m - i] - mp[i]);
+		}
+	}
+
+	if (mp[0])
+		cnt++;
+
+	cout << cnt << "\n";
+}
+// --> Another Approach
+
+/* int n, m;
+        cin >> n >> m;
+        map<int, int> cnt;
+        while (n--) {
+            int x;
+            cin >> x;
+            cnt[x % m]++;
+        }
+        int ans = 0;
+        for (auto &c : cnt) {
+            if (c.first == 0) ans++;
+            else if (2 * c.first == m) {
+                ans++;
+            } else if (2 * c.first < m || cnt.find(m - c.first) == cnt.end()) {
+                int x = c.second, y = cnt[m - c.first];
+                ans += 1 + max(0, abs(x - y) - 1);
+            }
+        }
+        cout << ans << '\n'; */
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
