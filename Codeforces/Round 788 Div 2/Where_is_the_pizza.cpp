361967504da25a9da3961e405a6464575358fe67
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-05-08 11:13:51 +0530

    Add files via upload

---
+++ Codeforces/Round 788 Div 2/Where_is_the_pizza.cpp
@@ -0,0 +1,169 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define N 2e5+7
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+
+ll inv(ll i) {
+	if (i == 1)
+		return 1;
+
+	return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;
+}
+
+ll mod_add(ll a, ll b) {
+	a = a % mod;
+	b = b % mod;
+
+	return (((a + b) % mod) + mod) % mod;
+}
+
+
+ll mod_mul(ll a, ll b, ll m = mod) {
+	a = a % m;
+	b = b % m;
+
+	return (((a * b) % m) + m) % m;
+}
+
+ll mod_sub(ll a, ll b, ll m = mod) {
+	a = a % m;
+	b = b % m;
+
+	return ((a - b) + m) % m;
+}
+
+// another binpow method
+ll pw(ll a, ll b, ll m) {
+	if (b == 0)
+		return 1;
+
+	if (b % 2 == 0) {
+		ll t = pw(a, b / 2, m);
+		return (1LL * t * t % m);
+	}
+	else {
+		ll t = pw(a, (b - 1) / 2, m);
+		t = (1LL * t * t) % m;
+		return (1LL * a * t % m);
+	}
+}
+
+ll mod_div(ll a, ll b, ll m = mod) {
+	a = a % m;
+
+	ll inv_b = pw(b, m - 2, m);
+	// inverse of b = b^m-2%m, only for prime m
+
+	ll res = a * inv_b % m;
+
+	return res;
+}
+
+// binary exponentiation
+// binpow
+// O(log(b))
+ll modulo(ll a, ll b, ll n = mod) {
+	ll x = 1, y = a;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			x = (x * y) % n; // multiplying with base
+		}
+		y = (y * y) % n; // squaring the base
+		b /= 2;
+	}
+	return x % n;
+}
+// binpow again
+ll pwr(ll a, ll b) {
+	a %= mod;
+	ll res = 1;
+
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return res;
+}
+
+vt<ll>adj, vis, m;
+
+ll dfs(int i) {
+	ll f = 0;
+	if (m[i])
+		f = 1;
+
+	if (vis[i])
+		return f;
+
+	vis[i] = 1;
+
+	return max(dfs(adj[i]), f);
+}
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	vt<ll>a(n), b(n), c(n);
+
+	for (ll i = 0; i < n; i++)
+		cin >> a[i];
+
+	for (ll i = 0; i < n; i++)
+		cin >> b[i];
+
+	for (ll i = 0; i < n; i++) {
+		cin >> c[i];
+	}
+
+	adj.clear();
+	adj = vt<ll>(n + 1);
+	vis.clear();
+	vis = vt<ll>(n + 1);
+	m.clear();
+	m = vt<ll>(n + 1);
+
+	for (ll i = 0; i < n; i++) {
+		adj[a[i]] = b[i];
+		m[c[i]] = 1;
+	}
+
+	ll ans = 1;
+
+	for (ll i = 1; i <= n; i++) {
+		if (!vis[i] and adj[i] != i) {
+			ll f = dfs(i);
+			if (!f)
+				ans = (ans * 2) % mod;
+		}
+	}
+
+	cout << ans << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
