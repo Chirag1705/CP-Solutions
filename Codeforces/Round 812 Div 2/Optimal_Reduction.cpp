3e3ea7483cb1bbaf0900c5c814d149b496621f19
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-08-11 15:43:52 +0530

    Create Optimal_Reduction.cpp

---
+++ Codeforces/Round 812 Div 2/Optimal_Reduction.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	vt<ll>vec1(n), vec2(n);
+
+	for (ll i = 0; i < n; i++)
+		cin >> vec1[i] >> vec2[i];
+
+	sort(vec1.begin(), vec1.end());
+	sort(vec2.begin(), vec2.end());
+
+	ll ans = 0;
+
+	if (vec1[n - 1] > 0)
+		ans += 2 * vec1[n - 1];
+	if (vec1[0] < 0)
+		ans += 2 * abs(vec1[0]);
+	if (vec2[n - 1] > 0)
+		ans += 2 * vec2[n - 1];
+	if (vec2[0] < 0)
+		ans += 2 * abs(vec2[0]);
+
+	cout << ans << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
