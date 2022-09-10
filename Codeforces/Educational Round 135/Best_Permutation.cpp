befc41df662e9ebaf22b99547d29f801842e76dc
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-09-10 19:31:59 +0530

    Create Best_Permutation.cpp

---
+++ Codeforces/Educational Round 135/Best_Permutation.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	vt<ll>arr(n), pref(n);
+
+	arr[n - 1] = n;
+	arr[n - 2] = n - 1;
+	arr[n - 3] = 1;
+
+	ll j = 2;
+
+	for (ll i = 0; i <= (n - 4); i++) {
+		arr[i] = j;
+		j++;
+	}
+
+	ll x = 0;
+
+	for (ll i = 0; i < n; i++) {
+		if (x < arr[i])
+			x += arr[i];
+		else
+			x = 0;
+	}
+
+	if (x == 0)
+		swap(arr[0], arr[1]);
+
+	for (auto i : arr)
+		cout << i << " ";
+	cout << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
