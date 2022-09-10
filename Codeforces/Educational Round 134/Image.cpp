7fb572408351ec174fe346736ea3ed5120cbf745
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-09-10 19:24:58 +0530

    Create Image.cpp

---
+++ Codeforces/Educational Round 134/Image.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	ll cnt = 0;
+
+	if (s1[0] == s2[0])
+		cnt++;
+
+	if (s1[0] == s2[1])
+		cnt++;
+
+	if (s1[0] == s1[1])
+		cnt++;
+
+	if (s1[1] == s2[0])
+		cnt++;
+
+	if (s1[1] == s2[1])
+		cnt++;
+
+	if (s2[0] == s2[1])
+		cnt++;
+
+	if (cnt == 6)
+		cout << "0" << "\n";
+	else if (cnt == 3 or cnt == 2)
+		cout << "1" << "\n";
+	else if (cnt == 1)
+		cout << "2" << "\n";
+	else
+		cout << "3" << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
