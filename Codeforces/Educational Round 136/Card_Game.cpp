2ca5b8a154f25aacb8e5df559eac135bbabae9f1
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-10-01 18:39:28 +0530

    Update Card_Game.cpp

--- Codeforces/Educational Round 136/Card_Game.cpp
+++ Codeforces/Educational Round 136/Card_Game.cpp
@@ -98,3 +98,136 @@ int main()
 		solve();
 	}
 }
+
+--------------------------------------------------------------------------------------------------------
+	
+	
+// Author : Epsilon573
+// If it works, don't touch it.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod  = 998244353;
+const ld eps  = 1e-9 ;
+const ll maxn = 105;
+const ll inf  = 5e18 ;
+const ll minf = -inf ;
+
+#define mp make_pair
+#define pb push_back
+#define endl "\n"
+
+ll fac[maxn],invfact[maxn];
+
+ll power(ll x, ll y) 
+{ 
+    ll res = 1; 
+    x = x % mod; 
+  
+    while (y > 0) { 
+        if (y & 1) 
+            res = (res * x) % mod; 
+        y = y >> 1; 
+        x = (x * x) % mod; 
+    } 
+
+    return res; 
+} 
+
+ll modInverse(ll n) 
+{ 
+    return power(n, mod - 2); 
+} 
+  
+void comp()
+{
+    fac[0] = 1, invfact[0] = 1; 
+    
+    for (ll i = 1; i < maxn; i++){ 
+        fac[i] = (fac[i - 1] * i) % mod;
+        invfact[i] = modInverse(fac[i]);
+    } 
+}
+
+ll NCR(ll n, ll r, ll p=mod) 
+{
+    if(r < 0 || n < 0)
+        assert(false);
+
+    if( n < r ) return 0;
+
+    if (r == 0 || r == n) 
+        return 1; 
+    
+    return ( fac[n] * invfact[r]  % mod) * invfact[n - r] % mod;  
+}
+
+void recurse(ll n, vector<vector<ll>>& dp){
+    if(n==2)
+        return;
+
+    recurse(n-2,dp);
+
+    dp[n][0] = (NCR(n-1,n/2) + dp[n-2][1]) % mod;
+    dp[n][1] = (NCR(n-2,n/2) + dp[n-2][0]) % mod;
+    dp[n][2] = (dp[n-2][2]) % mod;    
+
+
+}
+
+bool solve()
+{
+    ll n;
+    cin >> n;
+    vector<vector<ll>> dp(n+1,vector<ll> (3,-1));
+
+    // base cases
+    // alex wins
+    dp[2][0] = 1;
+    // boris wins
+    dp[2][1] = 0;
+    // draw
+    dp[2][2] = 1;
+
+    recurse(n,dp);
+    cout << dp[n][0] << " " << dp[n][1] << " " << dp[n][2] << endl;
+
+    return true;    
+}
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifdef EPSILON
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+    #endif
+
+    comp();
+
+    ll t=1;
+    cin >> t;
+
+    while(t--)
+    {
+        if(solve())
+        {
+            // cout << "YES" << endl;
+        }
+        else
+        {
+            // cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+} 
