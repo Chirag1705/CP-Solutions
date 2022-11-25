d1741f34c526788429e97e8daeb9461eb8d28122
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2022-11-25 20:57:23 +0530

    Create Factorise_n_plus_m.cpp

---
+++ Codeforces/Round 831 Div 2/Factorise_n_plus_m.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+ll inf = 1e18;
+
+void solve() {
+	string s;
+	cin >> s;
+
+	ll tmp = s[sz(s) - 1] - '0';
+
+	if (tmp == 2) {
+		cout << "2" << "\n";
+		return;
+	}
+
+	if (tmp == 3 && sz(s) == 1) {
+		cout << "3" << "\n";
+		return;
+	}
+
+	if (tmp == 5) {
+		cout << "3" << "\n";
+		return;
+	}
+
+	if (tmp == 1)
+		cout << "3" << "\n";
+	else if (tmp == 3)
+		cout << "2" << "\n";
+	else if (tmp == 7)
+		cout << "3" << "\n";
+	else
+		cout << "3" << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
+
