d6d046b228c98cd9a046ebeae577db69b8e7ef01
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-01-25 02:21:35 +0530

    Create make_it_round.cpp

---
+++ Codeforces/Round 834 Div 3/make_it_round.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+
+	ll ans = n, k = 1;
+
+	while (n % 10 == 0) {
+		n /= 10;
+	}
+
+	while (n % 5 == 0) {
+		n /= 5;
+		if (k * 2 <= m)
+			k *= 2;
+		else
+			break;
+	}
+
+	while (n % 2 == 0) {
+		n /= 2;
+		if (k * 5 <= m)
+			k *= 5;
+		else
+			break;
+	}
+
+	while (k * 10 <= m)
+		k *= 10;
+
+	k = (m / k) * k;
+
+	cout << ans*k << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
