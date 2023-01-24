cd1a036979641937f819972e89778cf94da6ae71
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-01-25 02:21:05 +0530

    Create thermostat.cpp

---
+++ Codeforces/Round 834 Div 3/thermostat.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+ll inf = 1e18;
+
+void solve() {
+	ll l, r, x;
+	cin >> l >> r >> x;
+
+	ll a, b;
+	cin >> a >> b;
+
+	if (a == b) {
+		cout << "0" << "\n";
+		return;
+	}
+
+	if (abs(a - b) >= x) {
+		cout << "1" << "\n";
+		return;
+	}
+
+	if ((abs(l - b) < x && abs(r - b) < x) || (abs(l - a) < x && abs(r - a) < x)) {
+		cout << "-1" << "\n";
+		return;
+	}
+
+	ll ans = INT_MAX;
+
+	if (abs(l - b) >= x) {
+		if (abs(a - l) >= x)
+			ans = min(ans, 2LL);
+		else
+			ans = min(ans, 3LL);
+	}
+
+	if (abs(r - b) >= x) { // (r-b)
+		if (abs(a - r) >= x)
+			ans = min(ans, 2LL);
+		else
+			ans = min(ans, 3LL);
+	}
+
+	cout << ans << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
+
