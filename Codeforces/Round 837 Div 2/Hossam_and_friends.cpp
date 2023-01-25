4e15770ffcb5497d236d43bd4906db0fe3bb8948
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-01-25 11:56:36 +0530

    Create Hossam_and_friends.cpp

---
+++ Codeforces/Round 837 Div 2/Hossam_and_friends.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+
+	vt<pair<ll, ll>>vec(m);
+	map<ll, ll>mp;
+
+	for (ll i = 0; i < m; i++) {
+		cin >> vec[i].ff >> vec[i].ss;
+		if (vec[i].ff > vec[i].ss)
+			swap(vec[i].ff, vec[i].ss);
+	}
+
+	for (ll i = 1; i <= n; i++) {
+		mp[i] = INT_MAX;
+	}
+
+	for (ll i = 0; i < m; i++) {
+		mp[vec[i].ff] = min(mp[vec[i].ff], vec[i].ss);
+	}
+	// for (ll i = 1; i <= n; i++)
+	// 	cout << mp[i] << " ";
+	// cout << "\n";
+	ll i = n - 1, j = n;
+	ll ans = 0;
+	while (i >= 1) {
+		if (mp[i] != INT_MAX) {
+			if (mp[i] <= j) {
+				j = mp[i] - 1;
+				if (i == j)
+					i--;
+			}
+			else {
+				ans += j - i;
+				i--;
+			}
+		}
+		else {
+			ans += j - i;
+			i--;
+		}
+	}
+
+	cout << ans + n << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
