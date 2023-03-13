eeb8e5312153128d494947de6083127f19cde60f
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-03-14 02:35:37 +0530

    Create Serval_and_Toxels_Arrays.cpp

---
+++ Codeforces/Round 853 Div 2/Serval_and_Toxels_Arrays.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+
+	vt<ll>arr(n);
+	unordered_map<ll, ll>mp;
+
+	for (ll i = 0; i < n; i++) {
+		cin >> arr[i];
+		mp[arr[i]] = m + 1;
+	}
+
+	ll ans = (m * (m + 1)) * n;
+	for (ll i = 0; i < m; i++) {
+		ll x, y;
+		cin >> x >> y;
+
+		ll val = arr[x - 1];
+		mp[val] -= (m - i);
+		mp[y] += (m - i);
+		arr[x - 1] = y;
+	}
+
+	for (auto i : mp) {
+		ans -= ((i.ss) * (i.ss - 1)) / 2;
+	}
+
+	cout << ans << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
