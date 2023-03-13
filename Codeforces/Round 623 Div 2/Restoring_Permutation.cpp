550f3d4ab6c337ddba7e3339f45baa221f5ff409
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-03-14 02:39:42 +0530

    Create Restoring_Permutation.cpp

---
+++ Codeforces/Round 623 Div 2/Restoring_Permutation.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	ll n;
+	cin >> n;
+
+	vt<ll>arr(n);
+	unordered_map<ll, ll>vp;
+	unordered_map<ll, ll>mp, mp1;
+
+	for (ll i = 0; i < n; i++) {
+		cin >> arr[i];
+		mp1[arr[i]]++;
+	}
+
+	ll i = 0;
+
+	while (i < n) {
+		ll j = arr[i] + 1;
+		bool f = 0;
+		while (j <= 2 * n) {
+			if (arr[i] == j || mp1[j]) {
+				j++;
+				continue;
+			}
+
+			ll tmp = min(arr[i], j);
+			if (tmp != arr[i]) {
+				cout << "-1" << "\n";
+				return;
+			}
+			vp[arr[i]] = j;
+			mp1[j]++;
+			i++;
+			f = 1;
+			break;
+		}
+		if (!f) {
+			cout << "-1" << "\n";
+			return;
+		}
+	}
+
+	for (ll i = 0; i < n; i++) {
+		cout << arr[i] << " " << vp[arr[i]] << " ";
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
