8704954c4abb1767b90d7a56395efaec1fa35d2d
Chirag Agrawal <91147379+Chirag1705@users.noreply.github.com>
2023-03-14 02:24:50 +0530

    Create Box_Fitting.cpp

---
+++ Codeforces/Round 711 Div 2/Box_Fitting.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define FASTIO ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define vt vector
+#define sz(x) int(x.size())
+#define ff first
+#define ss second
+// #define mod 1e9 + 7
+const ll mod = 1e9 + 7;
+#define set_bits __builtin_popcountll
+
+// binpow
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+ll inv(ll i) { if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod; }
+
+ll mod_add(ll a, ll b) { a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll mod_mul(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+
+ll mod_sub(ll a, ll b, ll m = mod) { a = a % m; b = b % m; return ((a - b) + m) % m; }
+
+ll mod_div(ll a, ll b, ll m = mod) { a = a % m; ll inv_b = expo(b, m - 2, m); ll res = a * inv_b % m; return res; }
+
+void solve() {
+	int n, w;
+	int box_width;
+	cin >> n >> box_width;
+
+	multiset<int> st;
+	for (int i = 0; i < n; i++) {
+		cin >> w;
+		st.insert(w);
+	}
+
+	int height = 1, space_left = box_width;
+
+	while (!st.empty()) {
+		auto it = st.upper_bound(space_left);
+		if (it != st.begin()) {
+			it--;
+			int val = *it;
+			space_left -= val;
+			st.erase(it);
+		} else {
+			space_left = box_width;
+			height++;
+		}
+	}
+
+	cout << height << endl;
+}
+
+int main()
+{
+	FASTIO;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+}
